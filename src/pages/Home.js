2a4d58f2dbfd707cb91946aed51ce331368a9f9d
kkli2e <kkli2e@pantarei-design.com>
2024-03-20 22:42:09 +0800

    update landing peojects section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 import Contact from '../components/Contact'
 import background from "../assets/images/home/container_1.png"
 import arrow from "../assets/images/icons/arrow.png"
-// import reHome from "../assets/images/home/rehome.png"
+import reHome from "../assets/images/home/rehome.png"
 import badgeDesign from "../assets/images/home/badge_design.png"
 import efSet from "../assets/images/home/EF_SET.png"
-// import hult from "../assets/images/home/HULT.png"
+import hult from "../assets/images/home/HULT.png"
 import brochure from "../assets/images/home/brochure.png"
 import XHS from "../assets/images/home/XHS_LP.png"
 import arrowBlack from "../assets/images/icons/arrow_black.png"
@@ -64,19 +64,33 @@ const Home = () => {
 
       <div id="projects">
         <div className={`project ${classNames[0]}`}>
-          <div className='image'>
-            <img src={brochure} alt="brochure" loading="lazy" />
+          <div className='image' style={{position: "relative", zIndex: "1"}}>
+            <img src={hult} alt="HULT" loading="lazy" />
           </div>
           <div className='content'>
             <div className='wrapper container'>
-              <div className='hashtag'><span>#BRANDING</span><span>#PRINT</span><span>#GRAPHIC</span></div>
-              <div className='title'>Print design</div>
-              <p>Create visual concepts and develop the overall layout and production design for company such as advertisements, brochures, booklet, flyer...etc.</p>
-              <Link to="/projects/print"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
+              <div className='hashtag'><span>#BRANDING</span><span>#WEBDESIGN</span><span>#LANDINGPAGE</span><span>#UI</span><span>#UX</span></div>
+              <div className='title'>Landing Page / Web design</div>
+              <p>Plan to delivering top-notch quality UI/UX design for your Website, Landing page by using Figma and Adobe creative suite.</p>
+              <p>Having usability and user experience in mind I will do brainstorming and research about your project to deliver attractively, user-friendly, and interactive UI designs.</p>
+              <Link to="/projects/web-design"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
             </div>
           </div>
         </div>
         <div className={`project ${classNames[1]}`}>
+          <div className='image'>
+            <img src={reHome} alt="RE-Home App" loading="lazy" />
+          </div>
+          <div className='content'>
+            <div className='wrapper container'>
+              <div className='hashtag'><span>#BRANDING</span><span>#UI</span><span>#UXRESERCHER</span><span>#DESIGNER STRATEGY</span><span>#LOGODESIGN</span></div>
+              <div className='title'>Re-Home</div>
+              <p>Re-Home is a local animals adoption app that’s help users easily to find the matched pet to adopt. Re-Home app strives to reduce the homeless dog numbers.They offer a high level flow to simplify the adoption process.</p>
+              <Link to="/projects/re-home"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
+            </div>
+          </div>
+        </div>
+        <div className={`project ${classNames[2]}`}>
           <div className='image'>
             <img src={XHS} alt="XHS Post" loading="lazy" />
           </div>
@@ -86,20 +100,6 @@ const Home = () => {
               <div className='title'>Digital design</div>
               <p>Create an ideas and strategies into engaging multimedia content, to support digital marketing campaigns across social media and digital platforms.</p>
               <Link to="/projects/digital"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
-            </div>
-          </div>
-        </div>
-
-        <div className={`project ${classNames[2]}`}>
-          <div className='image'>
-            <img src={badgeDesign} alt="Badge Design" loading="lazy" />
-          </div>
-          <div className='content'>
-            <div className='wrapper container'>
-              <div className='hashtag'><span>#BRANDING</span><span>#UI</span><span>#UXRESERCHER</span><span>#GRAPHIC</span></div>
-              <div className='title'>Badge Design</div>
-              <p>By offering badges as reward, its adds an element of challenge, competition, and achievement for users. This can motivate users to complete certain tasks.</p>
-              <Link to="/projects/badge-design"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
             </div>
           </div>
         </div>
@@ -116,7 +116,32 @@ const Home = () => {
             </div>
           </div>
         </div>
-
+        {/* <div className={`project ${classNames[4]}`}>
+          <div className='image'>
+            <img src={brochure} alt="brochure" loading="lazy" />
+          </div>
+          <div className='content'>
+            <div className='wrapper container'>
+              <div className='hashtag'><span>#BRANDING</span><span>#PRINT</span><span>#GRAPHIC</span></div>
+              <div className='title'>Print design</div>
+              <p>Create visual concepts and develop the overall layout and production design for company such as advertisements, brochures, booklet, flyer...etc.</p>
+              <Link to="/projects/print"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
+            </div>
+          </div>
+        </div> */}
+        {/* <div className={`project ${classNames[5]}`}>
+          <div className='image'>
+            <img src={badgeDesign} alt="Badge Design" loading="lazy" />
+          </div>
+          <div className='content'>
+            <div className='wrapper container'>
+              <div className='hashtag'><span>#BRANDING</span><span>#UI</span><span>#UXRESERCHER</span><span>#GRAPHIC</span></div>
+              <div className='title'>Badge Design</div>
+              <p>By offering badges as reward, its adds an element of challenge, competition, and achievement for users. This can motivate users to complete certain tasks.</p>
+              <Link to="/projects/badge-design"><img src={arrowBlack} alt="arrow" loading="lazy" className='arrow' /></Link>
+            </div>
+          </div>
+        </div> */}
       </div>
       <Container>
         <Contact />
